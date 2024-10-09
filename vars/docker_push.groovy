91d9da6d4f771fc4256a15de9e35d904d41c4d23
Abhishek-Kumar-Srivastava03a <139903506+Abhishek-Kumar-Srivastava03a@users.noreply.github.com>
2024-10-09 14:45:50 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,9 +1,6 @@
 def call(String Creds,tag,version){
   echo "this is test stage"
-  withCredentials([usernamePassword(
-      "credentialsId":"${creds}",
-      passwordVariable:"dockerPassword",
-      usernameVariable:"dockerUsername")]){
+  withCredentials([usernamePassword("credentialsId":"${creds}",passwordVariable:"dockerPassword",usernameVariable:"dockerUsername")]){
     sh "docker login -u ${dockerUsername} -p ${dockerPassword}"
     sh "docker image tag django-app:latest ${dockerUsername}/${tag}:${version}"
     sh "docker push ${dockerUsername}/${tag}:${version}"
