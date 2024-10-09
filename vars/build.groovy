9bc1c4185cc1a5c2c08bf78d581c571048ed37a2
Abhishek-Kumar-Srivastava03a <139903506+Abhishek-Kumar-Srivastava03a@users.noreply.github.com>
2024-10-09 14:36:32 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def call(String tag,version){
   echo "this is build stage"
   sh "whoami"
-  sh "docker build -t ${tag}:${version ."
+  sh "docker build -t ${tag}:${version}."
   echo "app build successful"
 }
