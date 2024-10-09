d7aea83e7275a29f20a60858a11c7526eb8e8f40
Abhishek-Kumar-Srivastava03a <139903506+Abhishek-Kumar-Srivastava03a@users.noreply.github.com>
2024-10-09 14:59:23 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,5 @@
 def call(String file_name){
   echo "this is deploy stage"
-  sh "docker compose -f ${file_name} down"
-  sh "docker-compose -f ${file_name} up -d"
+  sh "docker-compose -f ${file_name} down && docker-compose -f ${file_name} up -d"
   echo "deployment is done"
 }
